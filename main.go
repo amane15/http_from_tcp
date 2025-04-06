9eda42a0963f7ce6f54ff6cac33499419809265d
Atul Mane <maneatul225@gmail.com>
2025-04-06 10:36:29 +0000

    refactor to use channels

--- main.go
+++ main.go
@@ -15,27 +15,42 @@ func main() {
 		log.Fatalf("error while opening file: %v", err)
 	}
 
-	buffer := make([]byte, 8, 8)
-	currentLine := ""
+	linesChan := getLinesChannel(file)
 
-	for {
-		n, err := file.Read(buffer)
-		fmt.Println("Current Line ->", currentLine)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
+	for line := range linesChan {
+		fmt.Println("read:", line)
+	}
+}
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+		currentLine := ""
+		buffer := make([]byte, 8, 8)
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Fatalf("error while reading data: %v", err)
 				break
 			}
-			log.Fatalf("error while reading data: %v", err)
-			break
-		}
 
-		str := string(buffer[:n])
-		parts := strings.Split(str, "\n")
+			str := string(buffer[:n])
+			parts := strings.Split(str, "\n")
 
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Println("read:", currentLine+parts[i])
-			currentLine = ""
+			for i := 0; i < len(parts)-1; i++ {
+				lines <- currentLine + parts[i]
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
 		}
-		currentLine += parts[len(parts)-1]
-	}
+	}()
+
+	return lines
 }
